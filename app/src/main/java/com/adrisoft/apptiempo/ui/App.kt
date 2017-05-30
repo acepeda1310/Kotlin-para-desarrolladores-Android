3efda85ba92e80268e7ee20d206c1adfdf21851a
Adrián Cepeda Ávila <adriancepeda1310@gmail.com>
2017-05-30 23:46:31 +0200

    Captítulo 16. Aplicación singleton y propiedades delegadas

---
+++ app/src/main/java/com/adrisoft/apptiempo/ui/App.kt
@@ -0,0 +1,20 @@
+package com.adrisoft.apptiempo.ui
+
+/**
+ * Created by Adrián on 30/05/2017.
+ */
+
+import android.app.Application
+import com.adrisoft.apptiempo.ui.utiles.DelegacionesExt
+
+class App : Application() {
+
+    companion object {
+        var instance: App by DelegacionesExt.valorUnicoNoNull()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
---
+++ app/src/main/java/com/adrisoft/apptiempo/ui/utiles/DelegacionesExt.kt
@@ -0,0 +1,25 @@
+package com.adrisoft.apptiempo.ui.utiles
+
+/**
+ * Created by Adrián on 30/05/2017.
+ */
+
+import kotlin.reflect.KProperty
+
+object DelegacionesExt {
+    fun <T> valorUnicoNoNull() = VarValorUnicoNoNull<T>()
+}
+
+class VarValorUnicoNoNull<T> {
+
+    private var valor: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return valor ?: throw IllegalStateException("${property.name} no inicializada")
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, valor: T) {
+        this.valor = if (this.valor == null) valor
+        else throw IllegalStateException("${property.name} ya está inicializada")
+    }
+}
